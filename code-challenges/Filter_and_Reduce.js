fd007dc03bb86a1ec2df078fc40d26eea1ab7352
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-11-29 20:47:26 -0500

    Add completed Filter_and)Reduce.js exercises

--- code-challenges/Filter_and_Reduce.js
+++ code-challenges/Filter_and_Reduce.js
@@ -1,30 +1,30 @@
-/*
+//Task 1 - Using filter to Extract Specific Elements
+//Write a JavaScript function that uses the filter method to extract all the numbers greater than 10 from an array.
 
-Task 1 - Using filter to Extract Specific Elements
-Write a JavaScript function that uses the filter method to extract all the numbers greater than 10 from an array.
-
-*/
 console.log("-------- TASK 1 --------");
 
 const arrayNums = [5, 10, 12, 3, 18, 130, 44];
 const overTen = arrayNums.filter(num => num > 10);
 console.log(overTen); // 12, 18, 130, 44
 
-/*
+//Task 2 - Using reduce to Calculate the Sum of Numbers
+//Write a JavaScript function that uses the reduce method to calculate the sum of all numbers in an array.
 
-Task 2 - Using reduce to Calculate the Sum of Numbers
-Write a JavaScript function that uses the reduce method to calculate the sum of all numbers in an array.
+console.log()
+console.log("-------- TASK 2 --------");
+function sumNumbers(arr){
+  const total = 0;
+  const sum = arr.reduce((tot, value) => tot + value, total);
 
-*/
-//console.log()
-//console.log("-------- TASK 2 --------");
-//function sumNumbers(){
-//}
+  return sum;
+}
 
+console.log(sumNumbers([3,3,3,3,8])) // 20
+console.log(sumNumbers([2,2,2,2,2])) // 10
+console.log(sumNumbers([5,5])) // 10
 
-/*
-Task 3 - Create an Array of Unique Categories for a Restaurant Menu (reduce)
-Extract unique categories from a list of menu items.
+//Task 3 - Create an Array of Unique Categories for a Restaurant Menu (reduce)
+//Extract unique categories from a list of menu items.
 
 const menuItems = [
  { name: 'Pizza', category: 'Main Dish' },
@@ -34,18 +34,21 @@ const menuItems = [
  { name: 'Greek Salad', category: 'Salad' },
 ];
 
-*/
-//console.log()
-//console.log("-------- TASK 3 --------");
+console.log()
+console.log("-------- TASK 3 --------");
+const uniqueCategories = menuItems.reduce((unique, category) => {
+  if (!unique.includes(category.category)) {
+    unique.push(category.category)
+  }
+  return unique;
+}, []);
 
+console.log(uniqueCategories)
 
-/*
+// Task 4 - Count Coffee Shops by Neighborhood (filter)
+// Imagine you have collected data on various coffee shops in Chicago, including their neighborhoods. 
+// You want to analyze this data to understand the distribution of coffee shops across different neighborhoods.
 
-Task 4 - Count Coffee Shops by Neighborhood (filter)
-Imagine you have collected data on various coffee shops in Chicago, including their neighborhoods. 
-You want to analyze this data to understand the distribution of coffee shops across different neighborhoods.
-
-*/
 const coffeeShops = [
  { name: 'Cafe A', neighborhood: 'Rogers Park' },
  { name: 'Cafe B', neighborhood: 'Wicker Park' },
@@ -54,11 +57,18 @@ const coffeeShops = [
  { name: 'Cafe E', neighborhood: 'Rogers Park' },
 ];
 
-//console.log()
-//console.log("-------- TASK 4 --------");
-// const distribution = coffeeShops.filter((shops, i) => shops.neighborhood);
-// console.log(distribution);
+console.log()
+console.log("-------- TASK 4 --------");
+const neighborhoods = coffeeShops.map(n => n.neighborhood);
+const uniqueNeighborhoods = [...new Set(neighborhoods)];
 
+const neighborhoodObject = {};
+
+uniqueNeighborhoods.forEach(neighborhood => {
+  neighborhoodObject[neighborhood] = coffeeShops.filter(n => n.neighborhood === neighborhood).length;
+});
+
+console.log(neighborhoodObject);
 
 // ---------------------------------------------------------------
 // SUPPLEMENTAL LEARNING
