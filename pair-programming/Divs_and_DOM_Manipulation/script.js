f3bc3e3b1728434f9c7fffd96d3dd6d00c71d8d5
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-01 11:56:08 -0500

    Add completed index.html and script.js for this exercise

--- pair-programming/Divs_and_DOM_Manipulation/script.js
+++ pair-programming/Divs_and_DOM_Manipulation/script.js
@@ -2,3 +2,13 @@
 Add your javascript implementation here for each task.
 Remember to consult your instructions! 
 */
+
+const list = document.getElementById('list');
+const firstListItem = list.querySelector('li');
+firstListItem.textContent = "I'm learning Js"
+
+const listAll = list.querySelectorAll('li')
+const newItem = document.createElement('li')
+newItem.textContent = 'Chicken, rice, and broccli'
+list.appendChild(newItem)
+
