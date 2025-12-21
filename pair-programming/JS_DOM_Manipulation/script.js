5341c65eba1d724be3cf5aa9a129af447177c19c
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-21 10:09:01 -0500

    Add completed script.js for JS DOM Manipulation exercise

--- pair-programming/JS_DOM_Manipulation/script.js
+++ pair-programming/JS_DOM_Manipulation/script.js
@@ -8,11 +8,23 @@ function isValidColor(string) {
 // ========== DO NOT MODIFY ABOVE FUNCTION ===========
 
 // Event Listeners
-
+const bodytag = document.getElementById('bodytag');
+const element = document.getElementById('element');
 // Color Prompt
-
+const color = prompt("What color would you like to change the background to?");
 // Use color prompt to change background color
-
+if (isValidColor(color)) {
+  bodytag.style.backgroundColor = color;
+} else alert("That is an invalid color, please try again!");
 // Add an event listener for the mouse enter
-
+element.addEventListener("mouseenter", (event) => {
+  element.textContent = "Mouse enter event";
+  element.style.border = '2px solid black';
+  console.log("Mouse enter event.");
+});
 // Add an event listener for the mouse leave
+element.addEventListener("mouseleave", (event) => {
+  element.textContent = "Hover Me";
+  element.style.border = 'none';
+  console.log("Mouse leave event.");
+});
