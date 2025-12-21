c72e33a5c0f5137e1e3e8b5cca28d12bba9baae8
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2025-12-20 23:19:08 -0500

    Add completed find_all_anagrams exercise

--- code-challenges/find_all_anagrams.js
+++ code-challenges/find_all_anagrams.js
@@ -1,6 +1,8 @@
 /*
 
-An anagram is a word formed by rearranging the letters of a different word using all the original letters exactly once. The function is given two strings: s - to search in, p - a template word. Find the starting indexes of anagrams of p among substrings of the given s.
+An anagram is a word formed by rearranging the letters of a different word using all the original letters exactly once. 
+The function is given two strings: s - to search in, p - a template word. 
+Find the starting indexes of anagrams of p among substrings of the given s.
 
 Examples ==> function (s, p)
 findAnagrams("cbaebabacd", "abc") ➞ [0, 6]
@@ -11,7 +13,27 @@ findAnagrams("abab", "ab") ➞ [0, 1, 2]
 
 */
 
-// console.log(findAnagrams("cbaebabacd", "abc"));// [0, 6];
-// console.log(findAnagrams("abab", "ab")); // [0, 1, 2];
-// console.log(findAnagrams('a', 'ab')); // [];
+function findAnagrams (str, p) {
+    const pLen = p.length; // 3
+    let count = pLen;
+    let found = [];
+    p = p.split('').sort().join('');
+
+    if (str.length < pLen) return found;
+
+    for (let i = 0; i < str.length; i++){
+        let compare = str.slice(i, count).split('').sort().join('');
+        //console.log(compare);
+
+        if (compare === p) {
+            found.push(i);
+        }
+        count++;
+    } 
+    return found;
+}
+
+console.log(findAnagrams("cbaebabacd", "abc"));// [0, 6];
+console.log(findAnagrams("abab", "ab")); // [0, 1, 2];
+console.log(findAnagrams('a', 'ab')); // [];
 console.log(findAnagrams("racecar", "ace")); // [ 1, 3 ]
