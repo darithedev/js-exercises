36e64333873dbd7913920cbda601c725ab8ab2d8
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-01-14 22:39:41 -0500

    Add completed return the num exercise for weds with solution

---
+++ supplemental/weds-js-problems/return-the-next-number.js
@@ -0,0 +1,21 @@
+/*  Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+    Examples:
+    addition(0) ➞ 1
+
+    addition(9) ➞ 10
+
+    addition(-3) ➞ -2
+    
+    Notes:
+    Don't forget to return the result.
+*/
+
+// Add function addOne (num) that takes a number as argument 
+function addOne(num) {
+    // within function add one to num and return 
+    return num+=1;
+}
+
+console.log(addOne(0)) // 1
+console.log(addOne(9)) // 10
+console.log(addOne(-3)) // -2
